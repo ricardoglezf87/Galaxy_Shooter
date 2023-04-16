963d77c8f8fe0f849ae600dda7a04580ab2b8a2e
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2023-04-16 03:57:09 +0200

    Cooldown

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 10.0f;
+    private float _speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +16,12 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * Time.deltaTime * _speed);
 
         if(transform.position.y > 5.5f)
         {
             Destroy(gameObject);
         }
+
     }
 }
